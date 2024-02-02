833ac7144429832f25aea2e88abe2e7d5bb2d67d
XIN <60823033+anxndsgn@users.noreply.github.com>
2024-02-03 00:00:03 +0800

    Add project data to projects.js

---
+++ src/projects.js
@@ -0,0 +1,47 @@
+const projectData = [
+  {
+    projectName: "Birdy",
+    img: "/Birdy.webp",
+    link: "https://competition.adesignaward.com/design.php?ID=149441",
+  },
+  {
+    projectName: "Flipping",
+    img: "Flipping.webp",
+    link: "https://youtu.be/utkkCsSskik",
+  },
+
+  {
+    projectName: "Arduino Mini",
+    img: "Arduino.webp",
+    link: "https://github.com/anxndsgn/Arduino-mini",
+  },
+  {
+    projectName: "Simpread",
+    img: "Simpread.webp",
+    link: "http://ksria.com/simpread/docs/#/Sync?id=logo-%e8%ae%be%e8%ae%a1",
+  },
+  {
+    projectName: "QDUYVA",
+    img: "QDUYVA.webp",
+  },
+  {
+    projectName: "QGT",
+    img: "QGT.webp",
+  },
+  {
+    projectName: "Myoland",
+    img: "Myoland.webp",
+    link: "https://myoland.com",
+  },
+  {
+    projectName: "MarkMark",
+    img: "MarkMark.webp",
+  },
+  {
+    projectName: "OpenWear",
+    img: "OpenWear.webp",
+    link: "https://openwear.xyz",
+  },
+];
+
+export { projectData };
