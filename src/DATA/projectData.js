a32d03e83fa5cc1c4e21019885be8295b35ca770
XIN <60823033+anxndsgn@users.noreply.github.com>
2024-04-05 15:05:24 +0800

    test

---
+++ src/DATA/projectData.js
@@ -0,0 +1,85 @@
+const projectData = [
+  {
+    projectName: "Birdy",
+    id: "birdy",
+    thumbnail: "/thumbnails/Birdy.webp",
+    link: {
+      aDesign: "https://competition.adesignaward.com/design.php?ID=149441",
+    },
+    description: [
+      "Birdy is a speaker that is designed to evoke a tactile and interactive experience with a creature, rather than a geometric object. With intuitive controls, users can manage their music playback by using the bird as a game stick. By swinging it in different directions, users can adjust the volume, skip to the next or previous song, and more. In addition to its music-playing capabilities, Birdy also doubles as a smart home assistant that responds to various bird calls. Its aim is to create a more natural and emotionally engaging user experience.",
+      "The design of Birdy is inspired by the way bird breeders often stroke and interact with their pets. The bird is located at the top and the bottom is a conical speaker. It is designed to foster emotional connections between users and the device, creating a more natural and seamless user experience.",
+      "Birdy provides a more engaging way to control music playback. With its bird-shaped game stick, users can interact with their music in a unique and interesting way, making the experience feel more natural and intuitive. Designed to emulate the experience of touching and interacting with a live creature, Birdy provides a refreshing departure from traditional, lifeless speakers.",
+    ],
+    img: [
+      "/imgs/birdy/main.jpg",
+      "/imgs/birdy/1.jpg",
+      "/imgs/birdy/2.jpg",
+      "/imgs/birdy/3.jpg",
+      "/imgs/birdy/4.jpg",
+    ],
+  },
+  {
+    projectName: "Flipping",
+    video: "https://youtu.be/utkkCsSskik?si=riT74nW3bX8Wi5jn",
+    id: "flipping",
+    thumbnail: "/thumbnails/Flipping.webp",
+    link: "https://youtu.be/utkkCsSskik",
+    description: [],
+  },
+
+  {
+    projectName: "Arduino Mini",
+    id: "arduinomini",
+    thumbnail: "/thumbnails/Arduino.webp",
+    link: {
+      github: "https://github.com/anxndsgn/Arduino-mini",
+      easyEDA: "https://oshwhub.com/camcyano/arduinomini",
+    },
+    description: [
+      "The Arduino Mini is a small microcontroller board intended for use on breadboards and when space is at a premium. The revision 05 has a new package for the ATmega328, which enables all components to be on the top of the board. It also has an onboard reset button. The 05 version has the same pin configuration as revision 04.",
+      "The Minimum Arduino board, based on ATmega328P.",
+      "The 0.1 version is on EasyEDA.",
+    ],
+  },
+  {
+    projectName: "Simpread",
+    id: "simpread",
+    thumbnail: "/thumbnails/Simpread.webp",
+    link: "http://ksria.com/simpread/docs/#/Sync?id=logo-%e8%ae%be%e8%ae%a1",
+    description: [],
+  },
+  // {
+  //   projectName: "QDUYVA",
+  //   id: "quyda",
+  //   img: "/QDUYVA.webp",
+  // },
+  {
+    projectName: "QGT",
+    id: "qgt",
+    thumbnail: "/thumbnails/QGT.webp",
+    description: [],
+  },
+  {
+    projectName: "Myoland",
+    id: "myoland",
+    thumbnail: "/thumbnails/Myoland.webp",
+    link: "https://myoland.com",
+    description: [],
+  },
+  {
+    projectName: "MarkMark",
+    id: "markmark",
+    thumbnail: "/thumbnails/MarkMark.webp",
+    description: [],
+  },
+  {
+    projectName: "OpenWear",
+    id: "openwear",
+    thumbnail: "/thumbnails/OpenWear.webp",
+    link: "https://openwear.xyz",
+    description: [],
+  },
+];
+
+export { projectData };
